14f1da9dac84c8ad9b03a04d6fc6733800d89b6f
hamasugar <hamazi511@gmail.com>
2018-09-27 14:27:48 +0900

    Initial Commit

---
+++ MVCpractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVCpractice
+//
+//  Created by user on 2018/09/27.
+//  Copyright © 2018年 hamasugartanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
